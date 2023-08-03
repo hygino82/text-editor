d94514991aea36a504257580a985cc794752cf7e
Adroaldo <hygino@msn.com>
2023-08-03 18:45:52 -0300

    Método do Menu Arquivo > Salvar

--- Form1.cs
+++ Form1.cs
@@ -29,12 +29,54 @@ namespace EditorTXT
 
         private void mArquivoSalvar_Click(object sender, EventArgs e)
         {
+            if (File.Exists(Gerenciador.FilePath))
+            {
+                SalvarArquivo(Gerenciador.FilePath);
+            }
+            else
+            {
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.Title = "Salvar...";
+                dialog.Filter = "rich text file|*.rtf|texto|*.txt|todos|*.*";
+                dialog.CheckFileExists = false;
+                dialog.CheckPathExists = true;
 
+                var result = dialog.ShowDialog();
+
+                if (result != DialogResult.Cancel && result != DialogResult.Abort)
+                {
+                    SalvarArquivo(dialog.FileName);
+                }
+            }
         }
 
         private void mArquivoSalvarComo_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void SalvarArquivo(string path)
+        {
+            //Objeto responsável por escrever o arquivo
+            StreamWriter writer = null;
+            try
+            {
+                writer = new StreamWriter(path, false);
+                writer.Write(txtConteudo.Text);
+
+                FileInfo file = new FileInfo(path);
+                Gerenciador.FolderPath = file.DirectoryName + "\\";
+                Gerenciador.FileName = file.Name.Remove(file.Name.LastIndexOf("."));
+                Gerenciador.FileExt = file.Extension;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao Salvar o arquivo:\n" + ex.Message);
+            }
+            finally
+            {
+                writer.Close();
+            }
         }
 
         private void mArquivoSair_Click(object sender, EventArgs e)
